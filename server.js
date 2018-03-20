4eb1e5dd1f59005cb95af956d1936a5c24166a7e
JESS MCGOWAN (1703725) <1703725@rgu.ac.uk>
2018-03-20 13:11:06 +0000

    Modified redirect

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.get('/callback/', function(req, res) {
 
 
         // we can also pass the token to the browser to make requests from there
-        /*res.redirect('/#' +
+        res.redirect('/#' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
@@ -129,7 +129,7 @@ app.get('/callback/', function(req, res) {
           querystring.stringify({
             error: 'invalid_token'
           }));
-      }*/
+      }
     });
   }
 });
