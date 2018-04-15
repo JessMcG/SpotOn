5db40a01fa8f61693adb4dcf509596d67fab1dc4
NICKY TER MAAT (1715233) <1715233@rgu.ac.uk>
2018-04-15 16:37:35 +0100

    find user with user_id from db

--- server.js
+++ server.js
@@ -307,12 +307,36 @@ app.get('/profile', function(req, res) {
 app.get('/search', function(req, res) {
   console.log("Searching....");
   console.log("Session: " + req.session.session_id);
-  console.log("Access Tokekn: " + req.session.access_token);
+  console.log("Access Token: " + req.session.access_token);
 
   //TODO: Check if logged in
   // if(!req.session.loggedin){res.redirect('/login'); return;}
 
   var access_token = req.session.access_token;
+
+  db.collection('users').find({user_id: nickytermaat}).toArray(function(err, result) {
+    if (err) throw err;
+    //If user_id already exists, update the database
+    if (result.length>0){
+      console.log(db.collection('users').find({user_id: nickytermaat}));
+
+      // db.collection('users').update({user_id: body.id},{user_id: body.id, access_token: access_token, refresh_token: refresh_token}, function(err, result) {
+      //   if (err) throw err;
+      //   console.log('Saved to Database');
+      //   //add user details to current Session
+      //   req.session.user_id = body.id;
+      //   req.session.access_token = access_token;
+      //   req.session.loggedin = true;
+      //   console.log('session ID = '+ req.session.id);
+      //   console.log('session User ID = '+ req.session.user_id);
+      //   console.log('session Access Token = '+ req.session.access_token);
+      //   //redirect to home
+      //   res.redirect('/');
+    } else {
+      console.log("nickytermaat not found");
+    });
+    }
+
 
   var query = req.query.q;
   var type = req.query.type;
@@ -327,13 +351,29 @@ app.get('/search', function(req, res) {
     json: true
   };
 
+  request.get(searchoptions, function(error, response, body) {
+    console.log(body);
+
+    console.log("\nSEARCH RESULTS \n");
+    if (body.artists) {
+      for (var i = 0; i < body.artists.items.length; i++) {
+        console.log("\t ARTIST: " + body.artists.items[i].name);
+      }
+    } else if (body.tracks) {
+      for (var i = 0; i < body.tracks.items.length; i++) {
+        console.log("\t ARTIST: " + body.artists.items[i].name);
+      }
+    }
+
+    console.log(response.jsonData);
+  //=======
+    //console.log("SEARCH RESULTS \n" + "\tARTIST: " + body.artists + "\n\TRACK": body.track);
+  //>>>>>>> ac65cc00feb7d32f9724e36c500ca4e9f389a3b7
+  });
 
   request.post(authOptions, function(error, response, body) {
     if (!error && response.statusCode === 200) {
       res.send("Status 200 it worked");
-
-
-      console.log(response.jsonData);
 
       request.get(searchoptions, function(error, response, body) {
         console.log(body);
@@ -348,10 +388,14 @@ app.get('/search', function(req, res) {
             console.log("\t ARTIST: " + body.artists.items[i].name);
           }
         }
-//=======
+      //=======
         //console.log("SEARCH RESULTS \n" + "\tARTIST: " + body.artists + "\n\TRACK": body.track);
-//>>>>>>> ac65cc00feb7d32f9724e36c500ca4e9f389a3b7
+      //>>>>>>> ac65cc00feb7d32f9724e36c500ca4e9f389a3b7
       });
+
+      console.log(response.jsonData);
+
+
     }
   });
 });
