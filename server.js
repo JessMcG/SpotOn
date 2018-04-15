809efe36469c38270195d2e1f37dc5c0eb2bce94
mcsmall1 <mcsmall1@live.co.uk>
2018-04-15 10:59:16 +0100

    error testing

--- server.js
+++ server.js
@@ -369,6 +369,7 @@ app.get('/seedpl', function(req, res) {
   var output = '';
   var query = {'search.track':'1301WleyT98MSxVHPZCA6m'}
   db.collection('users').find(query).toArray, function(err, result) {
+  console.log('fart')
   console.log('result: '+result);
     if(!err){
       output = result.tracks;
