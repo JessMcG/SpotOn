45c0941c868768e72be0144f5e4d00c846585b25
nickytermaat <nickytermaat@users.noreply.github.com>
2018-04-16 09:54:23 +0100

    Add files via upload

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
    
-	$("#artistField").hide();
 	
+	$("#artistField").hide();
+	$(".playTrackButton2").hide();
+	
+	$(".playTrackButton").click(function(){
+		$(".playTrackButton").hide();
+		$(".playTrackButton2").show();
+    });
+	$(".playTrackButton2").click(function(){
+		$(".playTrackButton").show();
+		$(".playTrackButton2").hide();
+    });
+		
 	$(".bySong").click(function(){
         $(".bySong").removeClass("inactiveButton");
         $(".bySong").addClass("activeButton");
@@ -42,9 +53,6 @@ $(document).ready(function(){
 	});
 	
 	
-	
-	$("ol li:even").css("background-color", "#fafafa");
-
 	$("#loginLink").click(function(){
 		 
 		$("#loginOverlay").fadeIn(300);
@@ -58,7 +66,108 @@ $(document).ready(function(){
 			
     }); 
 	
+	
+	
+	$("#respMenu").click(function(){
+		  $("#container").animate({marginLeft: '-70%'},300);
+		  $("#header").animate({marginLeft: '-70%'},300);
+		  $("#nav").animate({marginLeft: '-70%'},300);
+		  $("#respNav").animate({width: '70%'},300);
+		  $("#respNav").addClass("respNavBoxShadow");
+		  $("#respMenu").fadeOut(350);
+		  $("#closeRespMenu").fadeIn(450);
+		  $(".respContent").fadeIn(200);
+    }); 
+	
+	$("#closeRespMenu").click(function(){
+		  $("#container").animate({marginLeft: '0%'},300);
+		  $("#header").animate({marginLeft: '0%'},300);
+		  $("#nav").animate({marginLeft: '0%'},300);
+		  $("#respNav").animate({width: '0%'},300);
+		  $("#respMenu").fadeIn(450);
+		  $("#closeRespMenu").fadeOut(250);
+		  $("#respNav").removeClass("respNavBoxShadow");
+		  $(".respContent").fadeOut(200);
+    }); 
+
 });
 
+$.ajax({
+		url: 'data.json',
+		dataType: 'json',
+		type: 'get',
+		cashe: false,
+		success: function(data){
+			$(data.tracks).each(function(index, value){
 
-	
+
+				var appendTrack = "<li class='trackOnProfile'><p>" + value.artist_name + " - <span>" + value.track_name + " " + "</span></p><div class='playTrackProfile'><img src='img/play.png' alt='play track' />";
+
+				$("ol li:even").css("background-color", "#fafafa");
+
+				$("#allTracks").append(appendTrack);  	
+
+
+			});
+
+			var t = 0;
+			
+			$("#trackArt").attr("src",data.tracks.[t].album_art);
+			$("#coverArtBluredInner").attr("src",data.tracks[t].album_art);
+			$("#playerArtistName").html(data.tracks[t].artist_name);
+			$("#playerTrackName").html(data.tracks[t].track_name);
+			$("#playerPlaylistTitle").html(data.tracks[t].playlist_title);
+			$("#ffs").attr("src",data.tracks[t].track);				
+			var playListLength = data.tracks.length;
+
+			$("#playNext").click(function(){
+				t++;
+				if(t > playListLength +1){
+					t = 0;
+				}
+				else{
+					console.log(data.tracks[t].artist_name);
+					console.log(t);
+					$("#trackArt").attr("src",data.tracks[t].album_art);
+					$("#coverArtBluredInner").attr("src",data.tracks[t].album_art);
+					$("#playerArtistName").html(data.tracks[t].artist_name);
+					$("#playerTrackName").html(data.tracks[t].track_name);
+					$("#playerPlaylistTitle").html(data.tracks[t].playlist_title);
+					$("#ffs").attr("src",data.tracks[t].track);
+					$(".playTrackButton2").hide();
+					$(".playTrackButton").show();
+				}
+			});
+
+			$("#playPrev").click(function(){
+				t--;
+				if(t < 0){
+					t = playListLength +1;
+				}
+				else{
+					console.log(data.tracks[t].track);
+					console.log(t);
+					$("#trackArt").attr("src",data.tracks[t].album_art);
+					$("#coverArtBluredInner").attr("src",data.tracks[t].album_art);
+					$("#playerArtistName").html(data.tracks[t].artist_name);
+					$("#playerTrackName").html(data.tracks[t].track_name);
+					$("#ffs").attr("src",data.tracks[t].track);
+					$(".playTrackButton2").hide();
+					$(".playTrackButton").show();
+				}
+			});
+
+			$("audio").hide();
+
+			$(".playTrackButton").click(function(){
+
+				$("#ffs").trigger('play');
+
+			});
+			$(".playTrackButton2").click(function(){
+
+				$("#ffs").trigger('pause');
+
+			});
+		}
+	});
