661892e2a9b94cf8f877de8772ebae735ae0f147
NICKY TER MAAT (1715233) <1715233@rgu.ac.uk>
2018-04-16 10:29:49 +0100

    append body to searchresults

--- server.js
+++ server.js
@@ -307,12 +307,12 @@ app.get('/search', function(req, res) {
     if (body.artists) {
       for (var i = 0; i < body.artists.items.length; i++) {
         console.log("\t ARTIST: " + body.artists.items[i].name);
-        $("#test").append("ARTIST Body: " + body);
+        $("#searchResults").append("ARTIST Body: " + body);
       }
     } else if (body.tracks) {
       for (var i = 0; i < body.tracks.items.length; i++) {
         console.log("\t TRACK: " + body.tracks.items[i].name);
-        $("#test").append("TRACK Body: " + body);
+        $("#searchResults").append("TRACK Body: " + body);
       }
     }
   });
