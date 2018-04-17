c916b083a10cd74bc1a59218c193da5596831375
LEWIS MCCULLOUGH (1702733) <1702733@rgu.ac.uk>
2018-04-17 12:38:40 +0100

    db insert?

--- server.js
+++ server.js
@@ -432,7 +432,7 @@ app.get('/create_pl', function(req, res, body) {
         // req.session.playlist_id = body.id
         var parsedData = JSON.parse(body);
         var playlist_id = parsedData.id;
-        db.collection('users').update({user_id: user_id},{user_id: user_id, access_token: access_token, playlists: {playlist_id: playlist_id}}, function(err, result) {console.log(result);});
+        db.collection('users').insert({user_id: user_id},{playlists: {playlist_id: playlist_id}}, function(err, result) {console.log(result);});
         console.log('playlist_id: '+ playlist_id);
       } else {
         console.log('failed: ' + res.statusCode);
