a97ef75762190e18e3a966160af3c3445936bc64
LEWIS MCCULLOUGH (1702733) <1702733@rgu.ac.uk>
2018-04-17 11:46:57 +0100

    lol

--- server.js
+++ server.js
@@ -429,15 +429,13 @@ app.get('/create_pl', function(req, res, body) {
     request.post(options, function(err, res, body) {
       if(!err && res.statusCode === 201){
         console.log('success: ' + res.statusCode + ' ' + body);
-        if (body.id!=null){
-          var parsedData = JSON.parse(body);
-          console.log(parsedData);
-          var pl_id = parsedData.id;
-          console.log(parsedData["id"]);
-          // assign the body.id to req.session.playlist_id
-          // req.session.playlist_id = body.id
-          console.log('playlist_id: '+ pl_id);
-        };
+        var parsedData = JSON.parse(body);
+        console.log(parsedData);
+        var pl_id = parsedData.id;
+        console.log(parsedData["id"]);
+        // assign the body.id to req.session.playlist_id
+        // req.session.playlist_id = body.id
+        console.log('playlist_id: '+ pl_id);
       } else {
         console.log('failed: ' + res.statusCode);
       };
