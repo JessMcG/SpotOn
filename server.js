7fd514869a02732637394057ef68530a042c9b16
NICKY TER MAAT (1715233) <1715233@rgu.ac.uk>
2018-04-19 09:32:38 +0100

    stringify result

--- server.js
+++ server.js
@@ -349,7 +349,7 @@ app.get('/search', function(req, res) {
   if (user_id != null) {    // Requirement: valid user id in session
     db.collection('users').find({user_id: user_id}).toArray(function(err, result) {
       if (result.length > 0) {
-        console.log("User exists: " + result);
+        console.log("User exists: " + result.stringify);
 
         //db.collection('users').update({user_id: req.session.user_id})
       } else {
