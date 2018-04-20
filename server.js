1a6cb956e12882f4c1cc103a3a0b4fa9ae35c75f
JESS MCGOWAN (1703725) <1703725@rgu.ac.uk>
2018-04-20 13:50:31 +0100

    Added console logs for debug

--- server.js
+++ server.js
@@ -301,6 +301,7 @@ app.get('/profile_playlists', function(req, res) {
       if (!error && response.statusCode === 200) {
         //Get the details from each playlist and save as a variable
         playlists = body.items;
+        console.log(body.items);
 
       } else {
         //Log the error in the console
@@ -327,7 +328,7 @@ app.get('/profile_tracks', function(req, res) {
 
     // GET Trackss from Spotify
     request.get(options, function(error, response, body) {
-      //console.log(body.items);
+      console.log(body.items);
 
       //If no errors from the API request
       if (!error && response.statusCode === 200) {
