f13908ea09182712e642a99a3ca7437c1404074e
JESS MCGOWAN (1703725) <1703725@rgu.ac.uk>
2018-04-23 12:44:37 +0100

    Backup of searches before updating

--- server.js
+++ server.js
@@ -160,9 +160,12 @@ app.get('/callback/', function(req, res) {
           //Search database for the current user ID
           db.collection('users').find({user_id: body.id}).toArray(function(err, result) {
             if (err) throw err;
+
+            //Save user's searches from db before updating user details
+            var searches_from_db = result.searches;
             //If user_id already exists, update the database
             if (result.length>0){
-              db.collection('users').update({user_id: body.id},{user_id: body.id, display_name: display_name, image_url: image_url, access_token: access_token, refresh_token: refresh_token}, {upsert: true}, function(err, result) {
+              db.collection('users').update({user_id: body.id},{user_id: body.id, display_name: display_name, image_url: image_url, access_token: access_token, refresh_token: refresh_token, searches: searches_from_db}, {upsert: true}, function(err, result) {
                 if (err) throw err;
                 console.log('Saved to Database');
                 //add user details to current Session
