9e0777570221799d852563d4e296f18a8668ba67
NICKY TER MAAT (1715233) <1715233@rgu.ac.uk>
2018-04-26 16:54:06 +0100

    comment out images

--- public/js/script2.js
+++ public/js/script2.js
@@ -331,7 +331,7 @@ console.log("Receiving data from /search...");
 				console.log("Amount of results: " + data.artists.items.length);
 				for (var i = 0; i <data.artists.items.length; i++) {
 					appendSearchResults += "<article class='searchResult'>"
-					appendSearchResults += "<img class='searchResultImage' src='" + data.artists.items[i].images[0].url +"' 'alt=''/>"
+					//appendSearchResults += "<img class='searchResultImage' src='" + data.artists.items[i].images[0].url +"' 'alt=''/>"
 					appendSearchResults += "<h3 class='searchResultName' id='"+data.artists.items[i].id+"'>" + data.artists.items[i].name + "</h3>"
 					//appendSearchResults	+= "<p id='artist_id'>" + data.artists.items[i].id + "</p>"
 					appendSearchResults += "<div class='addTrack addSearchedTrack' ><img src='img/next.png' alt='add track' /></div>"
