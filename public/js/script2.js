d75774067650b1975070ad3a421e24e0b638694d
NICKY TER MAAT (1715233) <1715233@rgu.ac.uk>
2018-04-26 11:20:41 +0100

    log data

--- public/js/script2.js
+++ public/js/script2.js
@@ -178,6 +178,7 @@ console.log("Receiving data from /search...");
 		success: function(data){
 			$(data.tracks).each(function(index, value){
 				console.log("Received data from /search!");
+				console.log(data);
 
 				var appendTrack = "<li class='trackOnProfile'><p class='ellipsis' title='"+ value.artist_name + " - " + value.track_name  +"'>" + value.artist_name + " - <span>" + value.track_name + " " + "</span></p><div class='playTrackProfile'><img class='playSingleTrack' src='img/play.png' alt='" + value.track_id +"' />";
 
