24da9382c66063678e5f19fde4c49196da6d3272
NICKY TER MAAT (1715233) <1715233@rgu.ac.uk>
2018-04-26 17:39:26 +0100

    brackets..

--- public/js/script2.js
+++ public/js/script2.js
@@ -359,7 +359,7 @@ console.log("Receiving data from /search...");
  * If the searched artist is selected, its top tracks will be displayed. The artists id is sent from client to server, top tracks data is returned from server to client.
  */
 // Unfortunately, I was unable to get the click events working on dynamically generated elements. Sorry for that..
-$('#searchResult').on("click", function(e){var id=e.target.attr('id'); console.log(e); //getTopTracksFromArtist(id);});
+$('#searchResult').on("click", function(e){var id=e.target.attr('id'); console.log(e)}); //getTopTracksFromArtist(id);});
 function getTopTracksFromArtist() {
 	// Using hardcoded artist ID to be able to display some top tracks
 	var artistID = "12Chz98pHFMPJEknJQMWvI" //id;
